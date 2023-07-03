0c4d2f9e463c6995c8ce30252e405c528737baee
iqbalzaber <iqbalxaber@gmail.com>
2023-07-03 23:36:15 +0600

    done

---
+++ src/components/About.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            About
+          </p>
+        </div>
+
+    
+
+        <br />
+
+        <p className="text-xl">
+        I'm Jr Front end developer. Recently I know HTML, CSS, Tailwind, Bootstrap, JavaScript, React, Node, Mongo, Express, and much more. I have experience in building projects on the MERN stack.
+
+
+I have learned, from the very start, the importance of passion, dedication, confidence, and time management.
+
+Portfolio
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
